599ae4cb18927764396385d8a4e0eed6b2840683
2beone-jino <for1self@gmail.com>
2026-03-25 17:18:01 +0900

    project init

---
+++ widget/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+const FULL_URL = 'https://server.for1self.workers.dev/';
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), tailwindcss()],
+  base: mode === 'production' ? FULL_URL : undefined,
+  build: {
+    outDir: '../server/dist',
+    emptyOutDir: true,
+  },
+}));
