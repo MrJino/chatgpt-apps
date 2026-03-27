7f805a6c15e6e0eb6d4096476214d4d6a4aa877a
2beone-jino <for1self@gmail.com>
2026-03-27 14:20:36 +0900

    flashcards kv

--- widget/vite.config.ts
+++ widget/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-const FULL_URL = 'https://server.for1self.workers.dev/';
+const FULL_URL = 'https://flashcard-server.for1self.workers.dev/';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
